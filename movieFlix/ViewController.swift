f38929b09e86caf13b9fc1372d29efa79678519e
Erin Byrd <ebbyrd@email.unc.edu>
2021-02-20 00:15:49 -0500

    Initial Commit

---
+++ movieFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieFlix
+//
+//  Created by Erin Byrd on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
