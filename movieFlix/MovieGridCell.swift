643fe6b797d38b2d73a8d47a13ebe42d92cf9a19
Erin Byrd <ebbyrd@email.unc.edu>
2021-02-27 02:50:32 -0500

    Added a tab bar with a superhero movie grid view

---
+++ movieFlix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  movieFlix
+//
+//  Created by Erin Byrd on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- movieFlix/MoviesViewController.swift
+++ movieFlix/MoviesViewController.swift
@@ -62,10 +62,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
+        
         let synopsis = movie["overview"] as! String
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+
         
         let baseURL = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
@@ -77,14 +79,31 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up Details")
+        
+        //Find the selected movie
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
 
 }
